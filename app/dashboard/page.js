9c2dcfbb67daa6fe30aa95ad54cfb4cdf2ad497f
Anshul Karande <anshulkarande20@gmail.com>
2025-02-16 00:54:08 -0500

    pomodoro css

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,13 +2,18 @@ import PomodoroTimer from "../components/pomodoroTimer";
 import StreakTracker from "../components/streakTracker";
 import Leaderboard from "../components/leaderboard";
 
+import { Ripple } from "@/components/magicui/ripple";
+
 export function Dashboard() {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">Study Streak Dashboard</h1>
+    <div className="bg-black min-h-screen text-white">
+      <h1 className="flex flex-row justify-center items-center text-4xl font-bold h-72">
+        You miss all the shots that you don't take, so grind!!
+      </h1>
       <PomodoroTimer />
-      <StreakTracker />
-      <Leaderboard />
+      {/* <StreakTracker /> */}
+      {/* <Leaderboard /> */}
+      <Ripple />
     </div>
   );
 }
